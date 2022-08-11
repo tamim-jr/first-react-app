88ec323776385c9d4753c953f196343b610ffd8b
Tamim Mahmud <internationalinf24@gmail.com>
2022-08-11 20:26:47 +0600

    start coding in react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var nam = "Tamim Mahmud"
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +10,10 @@ function App() {
         <p>
           Edit done <code>src/App.js</code> and save to reload.
         </p>
+        <h2>Hello World in React !</h2>
+        <h3>Simple math 3+5= {3+5}</h3>
+        <h3>Heard Math (2+8)*4 + 15 = {(2+8)*4 + 15}</h3>
+        <h4>My name is {nam}</h4>
         <a
           className="App-link"
           href="https://reactjs.org"
