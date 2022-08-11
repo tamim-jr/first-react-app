9d2df236b08af3612ed9138e32edbb90bdbe4353
Tamim Mahmud <internationalinf24@gmail.com>
2022-08-11 23:28:39 +0600

    inout html and css in react app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  var nam = "Tamim Mahmud"
+  var person ={ name: "Tamim Mahmud",
+                age: 30,
+                profession: "Programing ",              
+}
+var style ={
+  color : "#61DAFB",
+  backgroundColor : "green",
+  padding: 15,
+  borderRadius : 8,
+}
   return (
     <div className="App">
       <header className="App-header">
@@ -10,10 +19,12 @@ function App() {
         <p>
           Edit done <code>src/App.js</code> and save to reload.
         </p>
-        <h2>Hello World in React !</h2>
+        <h2 style={style}>Hello World in React !</h2>
         <h3>Simple math 3+5= {3+5}</h3>
-        <h3>Heard Math (2+8)*4 + 15 = {(2+8)*4 + 15}</h3>
-        <h4>My name is {nam}</h4>
+        <h3 style={{backgroundColor : 'green', padding: 10, borderRadius: 8}}>Heard Math (2+8)*4 + 15 = {(2+8)*4 + 15}</h3>
+        <h4>My name is {person.name + " . My age is " + person.age}</h4>
+        <MyInfo></MyInfo>
+        <MyInfo></MyInfo>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -25,6 +36,22 @@ function App() {
       </header>
     </div>
   );
+}
+
+function MyInfo(){
+  var PersonStyle={
+    border: "2px solid red", 
+    margin: "5px", padding: "10px", 
+    backgroundColor: "cyan", 
+    color: "black", 
+    borderRadius: "8px"
+  }
+  return (
+  <div style= {PersonStyle}>
+  <h2>First Name: Tamim</h2>
+  <h2>Last Name: Mahmud</h2>
+  </div>
+  )
 }
 
 export default App;
