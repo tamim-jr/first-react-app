f767bd49133350a737eff32875fe3249ff783824
Tamim Mahmud <internationalinf24@gmail.com>
2022-08-12 22:29:55 +0600

    product grid in js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,20 +3,30 @@ import './App.css';
 
 function App() {
   const scholar = ["Zakir Nayek", "Mizanur Rahman", "Ahmad Ullah"]
-  var person ={ name: "Tamim Mahmud",
-                age: 30,
-                profession: "Programing ",              
-}
-var style ={
-  color : "#61DAFB",
-  backgroundColor : "green",
-  padding: 15,
-  borderRadius : 8,
-}
+  const Products = [
+    {name: "Photoshop", price: "45.00$"},
+    {name: "illustrator", price: "145$"},
+  ]
+
+  const ProductNames = Products.map(proName => Products.name);
+
+//   var person ={ name: "Tamim Mahmud",
+//                 age: 30,
+//                 profession: "Programing ",              
+// }
+// var style ={
+//   color : "#61DAFB",
+//   backgroundColor : "green",
+//   padding: 15,
+//   borderRadius : 8,
+// }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Product product={Products[0]}></Product>
+        <Product product={Products[1]}></Product>
+        {/* <Product proName={Products[1].name} price={Products[1].price}></Product> */}
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit done <code>src/App.js</code> and save to reload.
         </p>
@@ -26,55 +36,81 @@ var style ={
         <h4>My name is {person.name + " . My age is " + person.age}</h4>
         <MyInfo name=" Tamim Mahmud" age="31" profession="Programing"></MyInfo>
         <MyInfo name = "Rajjak" age=" 40" profession="Acting"></MyInfo>
-        <MyInfo name = {scholar[0]} age=" 50" profession="Islamic Scholar"></MyInfo>
-        <ShopInfo name="Tamim Com" add="Lohagara, Narail"></ShopInfo>
-        <ShopInfo name="Rohim Com" add="Lohagara, Narail"></ShopInfo>
-        <a
+        <MyInfo name = {scholar[0]} age=" 50" profession="Islamic Scholar"></MyInfo> */}
+        {/* <ShopInfo name="Tamim Com" add="Lohagara, Narail"></ShopInfo>
+        <ShopInfo name="Rohim Com" add="Lohagara, Narail"></ShopInfo> */}
+
+
+        {/* { <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> } */}
       </header>
     </div>
   );
 }
 
-function MyInfo(props){
-  var PersonStyle={
-    border: "2px solid red", 
-    margin: "5px", padding: "10px", 
-    backgroundColor: "cyan", 
-    color: "black", 
-    borderRadius: "8px"
-  }
-  return (
-  <div style= {PersonStyle}>
-  <h2>Name:{props.name}</h2>
-  <h2>Age: {props.age}</h2>
-  <h3>Profession: {props.profession}</h3>
-  </div>
-  )
-}
+// function MyInfo(props){
+//   var PersonStyle={
+//     border: "2px solid red", 
+//     margin: "5px", padding: "10px", 
+//     backgroundColor: "cyan", 
+//     color: "black", 
+//     borderRadius: "8px"
+//   }
+//   return (
+//   <div style= {PersonStyle}>
+//   <h2>Name:{props.name}</h2>
+//   <h2>Age: {props.age}</h2>
+//   <h3>Profession: {props.profession}</h3>
+//   </div>
+//   )
+// }
 
 
-function ShopInfo(props){
-  var ShopStyle={
-    border: "2px solid red", 
-    margin: "5px", padding: "10px", 
-    backgroundColor: "cyan", 
-    color: "black", 
-    borderRadius: "8px"
+// function ShopInfo(props){
+//   var ShopStyle={
+//     border: "2px solid red", 
+//     margin: "5px", padding: "10px", 
+//     backgroundColor: "cyan", 
+//     color: "black", 
+//     borderRadius: "8px"
+//   }
+//   return (
+//     <div style={ShopStyle}>
+//       <h2>Shop Name: {props.name}</h2>
+//       <h3>Address: {props.add}</h3>
+//       <h3>Service: Computer Work, sells, service</h3>
+//     </div>
+//   )
+// }
+
+
+function Product(props){
+  const ProductStyle = {
+    border: "1px solid gray",
+    borderRadius: "5px",
+    backgroundColor: "lightgray",
+    height: "300px",
+    width: "300px",
+    float : "left",
+    color: "black"
   }
+const {name, price} = props.product;
   return (
-    <div style={ShopStyle}>
-      <h2>Shop Name: {props.name}</h2>
-      <h3>Address: {props.add}</h3>
-      <h3>Service: Computer Work, sells, service</h3>
+    <div style={ProductStyle}>
+      {/* <h2>{props.product.name}</h2> */}
+      <h2>{name}</h2>
+      {/* <h1>Price: {props.product.price}</h1> */}
+      <h1>Price: {price}</h1>
+      <button>Buy Now</button>
     </div>
   )
 }
+
 
 export default App;
